f994724c9fdbe16cc9ebc0d5302cc4f1f0e5ac01
fabriciosantana <fabricio.santana@gmail.com>
2024-03-26 21:14:19 -0300

    colocando mais métodos

--- redis/app.py
+++ redis/app.py
@@ -32,61 +32,50 @@ def get(key):
     else:
         return f"{key} is not exists"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
-'''
-from flask import Flask
-import redis
-app = Flask(__name__)
-r = redis.Redis(host='redis', port=6379, db=0)
-
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World123!'
 
 @app.route('/redis')
 def redis():
     try:
-        r.set("alguma_chave", "algum_valor")
-        valor = r.get("alguma_chave")
+        db.set("alguma_chave", "algum_valor")
+        valor = db.get("alguma_chave")
         app.logger.info(f'Valor obtido do Redis: {valor}')
         return 'Olá, mundo!'
     except:
         return 'Erro'
 
-@app.route('/set/<chave>/<valor>')
+@app.route('/set_valor/<chave>/<valor>')
 def set_valor(chave, valor):
-    r.set(chave, valor)
+    db.set(chave, valor)
     return f'Valor {valor} foi armazenado com a chave {chave}'
 
-@app.route('/get/<chave>')
+@app.route('/get_valor/<chave>')
 def get_valor(chave):
-    valor = r.get(chave)
+    valor = db.get(chave)
     if valor:
         return f'Valor recuperado: {valor.decode("utf-8")}'
     else:
         return 'Chave não encontrada'
 
-@app.route('/push/<lista>/<valor>')
+@app.route('/push_lista/<lista>/<valor>')
 def push_lista(lista, valor):
-    r.lpush(lista, valor)
+    db.lpush(lista, valor)
     return f'Valor {valor} adicionado à lista {lista}'
 
-@app.route('/pop/<lista>')
+@app.route('/pop_lista/<lista>')
 def pop_lista(lista):
-    valor = r.lpop(lista)
+    valor = db.lpop(lista)
     if valor:
         return f'Valor retirado da lista {lista}: {valor.decode("utf-8")}'
     else:
         return f'Lista {lista} está vazia ou não existe'
 
-@app.route('/listar/<lista>')
+@app.route('/listar_valores/<lista>')
 def listar_valores(lista):
-    valores = r.lrange(lista, 0, -1)
+    valores = db.lrange(lista, 0, -1)
     return f'Valores na lista {lista}: {[valor.decode("utf-8") for valor in valores]}'
     
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-'''
